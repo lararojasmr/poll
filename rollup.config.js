c8ab44d9d8e5b18d61026ac03d9017092a1876c7
Manuel Rafael Lara Rojas <lararojas.mr@gmail.com>
2022-09-01 23:55:10 -0300

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import typescript from '@rollup/plugin-typescript'
 import { terser } from 'rollup-plugin-terser'
 
 /** @type {import('rollup').RollupOptions} */ const options = {
+  module: false,
   input: 'src/poll.ts',
   output: {
     dir: 'dist',
