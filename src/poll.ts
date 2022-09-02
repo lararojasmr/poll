034b0071ddec397f98b2c98409657b1e5e9e7e01
Manuel Rafael Lara Rojas <lararojas.mr@gmail.com>
2022-09-01 23:37:15 -0300

    Update poll.ts

--- src/poll.ts
+++ src/poll.ts
@@ -5,7 +5,7 @@
  * @param delayOrDelayCallback The delay (in milliseconds) to wait before calling the function again. Can be a function.
  * @param shouldStopPolling A callback function indicating whether to stop polling.
  */
-export async function poll(
+export default async function poll(
   fn: () => any,
   delayOrDelayCallback: number | (() => number),
   shouldStopPolling: () => boolean | Promise<boolean> = () => false
